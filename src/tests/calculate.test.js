4f9bcdf82c8c200b5192aaf93a0ca1b25e24971d
adebowale <ademibowaleadegboye@gmail.com>
2022-08-12 20:03:25 +0100

    fix linters

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,60 +1,56 @@
 import calculate from '../logic/calculate';
-describe("testing the calcultion logic",()=>{
-          test("testing first input",()=>{
-            let obj={
-                    total: null,
-                    next: null,
-                    operation: null,
-                  };
-                  let result=calculate(obj,"3")
-            expect(result.next).toBe("3")       
- }) 
 
- test("testing second input",()=>{
-          let obj={
-                  total: null,
-                  next: "5",
-                  operation: null,
-                };
-                let result=calculate(obj,"+")
-          expect(result.total).toBe("5")    
-          expect(result.operation).toBe("+")     
-}) 
+describe('testing the calcultion logic', () => {
+  test('testing first input', () => {
+    const obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    const result = calculate(obj, '3');
+    expect(result.next).toBe('3');
+  });
 
-test("testing third input",()=>{
-          let obj={
-                  total: "10",
-                  next: null,
-                  operation: "+",
-                };
-                let result=calculate(obj,"3")
-          expect(result.next).toBe("3")       
-}) 
+  test('testing second input', () => {
+    const obj = {
+      total: null,
+      next: '5',
+      operation: null,
+    };
+    const result = calculate(obj, '+');
+    expect(result.total).toBe('5');
+    expect(result.operation).toBe('+');
+  });
 
-test("testing total output",()=>{
-          let obj={
-                  total: "10",
-                  next: "3",
-                  operation: "+",
-                };
-                let result=calculate(obj,"=")
-          expect(result.total).toBe("13")       
-}) 
+  test('testing third input', () => {
+    const obj = {
+      total: '10',
+      next: null,
+      operation: '+',
+    };
+    const result = calculate(obj, '3');
+    expect(result.next).toBe('3');
+  });
 
-test("testing clear output",()=>{
-          let obj={
-                  total: "10",
-                  next: "3",
-                  operation: "+",
-                };
-                let result=calculate(obj,"AC")
-          expect(result.total).toBe(null)    
-          expect(result.next).toBe(null)  
-          expect(result.operation).toBe(null)     
-}) 
+  test('testing total output', () => {
+    const obj = {
+      total: '10',
+      next: '3',
+      operation: '+',
+    };
+    const result = calculate(obj, '=');
+    expect(result.total).toBe('13');
+  });
 
-
-
-
-
-})
+  test('testing clear output', () => {
+    const obj = {
+      total: '10',
+      next: '3',
+      operation: '+',
+    };
+    const result = calculate(obj, 'AC');
+    expect(result.total).toBe(null);
+    expect(result.next).toBe(null);
+    expect(result.operation).toBe(null);
+  });
+});
--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,19 +1,20 @@
 import operate from '../logic/operate';
-describe("testing the operations",()=>{
-          test("testing addition operation",()=>{
-            let result=operate(1,2,"+") 
-            expect(result).toBe("3")       
-          })
-          test("testing subtract operation",()=>{
-                    let result=operate(3,1,"-") 
-                    expect(result).toBe("2")       
-                  })
-                  test("testing multiplication operation",()=>{
-                    let result=operate(6,5,"x") 
-                    expect(result).toBe("30")       
-                  })
-                  test("testing division operation",()=>{
-                    let result=operate(30,5,"÷") 
-                    expect(result).toBe("6")       
-                  })
-})
+
+describe('testing the operations', () => {
+  test('testing addition operation', () => {
+    const result = operate(1, 2, '+');
+    expect(result).toBe('3');
+  });
+  test('testing subtract operation', () => {
+    const result = operate(3, 1, '-');
+    expect(result).toBe('2');
+  });
+  test('testing multiplication operation', () => {
+    const result = operate(6, 5, 'x');
+    expect(result).toBe('30');
+  });
+  test('testing division operation', () => {
+    const result = operate(30, 5, '÷');
+    expect(result).toBe('6');
+  });
+});
